b0a665da3ff795f3aceaa214cbd15eb5f108eede
doiy <38238615+doiy@users.noreply.github.com>
2023-10-14 21:53:10 +0900

    temp

---
+++ bmp_2/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <a href="https://docs.google.com/forms/d/e/1FAIpQLScCrdrGQ1fx_fDxqrXEo34cLNtq6KjcZB2WTv4O_yJDKEzgKg/viewform">
+        <img src={process.env.PUBLIC_URL+ "bmp.jpg"} className="bmp-image" alt="bmp"  />
+      </a>
+    </div>
+  );
+}
+
+export default App;
